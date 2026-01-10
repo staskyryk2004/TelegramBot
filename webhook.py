b209d3118d59c17a06532b947c76dbe244dd1d7e
Stas Kyryk <staskyryk@gmail.com>
2026-01-10 10:11:00 +0200

    Add files

--- webhook.py
+++
@@ -1,7 +0,0 @@
-import telebot
-import config
-Token='8304724754:AAFW9hbOFNGCPI0vqQiN3OvTqz2IIwYQsVI'
-webhook_url=f'https://rozklad.cx.ua/webhook/{Token}'
-client=telebot.TeleBot(config.config['token'])
-client.remove_webhook()
-client.set_webhook(url=webhook_url)
