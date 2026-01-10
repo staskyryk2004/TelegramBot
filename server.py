85639f5a6dddc946e7fdbb1004aa32acf1487cfa
Stas Kyryk <staskyryk@gmail.com>
2026-01-10 08:45:49 +0200

    Add files

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
+import telebot
+import config
 from flask import Flask, app, request
 from flask import send_from_directory
 
+Token='8304724754:AAFW9hbOFNGCPI0vqQiN3OvTqz2IIwYQsVI'
+client=telebot.TeleBot(config.config['token'])
+webhook_path=f'/webhook{Token}'
 app=Flask(__name__)
 
+@app.route(webhook_path, methods={'POST'})
+def webhook():
+   update=telebot.types.Update.de_json(request.get_data().decode('utf-8'))
+   client.process_new_updates([update])
+   return 'ok', 200
 @app.route('/local')
 def get_rozklad():
     return send_from_directory(
