ddf8b3939812a8a288cd18023ef5db36833a3fb7
Stas Kyryk <staskyryk@gmail.com>
2026-01-14 03:45:53 +0200

    Add files

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
 from flask import Flask, app, request
 from flask import send_from_directory
-
+import telebot
+import config
+client=telebot.TeleBot(config.config['token'])
 app=Flask(__name__)
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+   update=request.get_json()
+   if 'message' in update:
+    chat_id= update['message']['chat']['id']
+    client.send_message(chat_id, "Привіт з Webhook")
+    return 'ok'
 @app.route('/local')
 def get_rozklad():
     return send_from_directory(
