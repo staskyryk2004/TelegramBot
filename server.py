0d6e2708e4da41c911b87a50be41a54e01dbf07c
Stas Kyryk <staskyryk@gmail.com>
2026-07-11 18:02:53 +0300

    Add files

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ def home():
     return "Сервер працює стабільно!", 200
 @app.route('/local')
 
-@limiter.limit("2 per minute")
+@limiter.limit("10 per minute")
 def get_rozklad():
     return send_from_directory(
         directory='local',
