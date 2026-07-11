c4d496a294a3c80e4614eca31453adb52ec536fb
Stas Kyryk <staskyryk@gmail.com>
2026-07-11 20:19:28 +0300

    Add files

--- server.py
+++ server.py
@@ -9,13 +9,13 @@ app=Flask(__name__)
 limiter=Limiter(
     get_remote_address,
     app=app,
-    default_limits=["10 per minute", "20 per hour"],
     storage_uri="memory://")
 
 @app.route('/')
 def home():
     return "Сервер працює стабільно!", 200
 @app.route('/local')
+@limiter.limit("10 per minute", "20 per hour")
 def get_rozklad():
     return send_from_directory(
         directory='local',
